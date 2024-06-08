7ab546adcec3bbf04db1804264a5c363f8f07fbe
Vaibhav171717 <153853106+Vaibhav171717@users.noreply.github.com>
2024-06-08 14:26:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/taskmanagement', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  dueDate: Date
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const newTask = new Task(req.body);
+  await newTask.save();
+  res.status(201).json(newTask);
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.json(task);
+});
+
+app.put('/tasks/:id', async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTask);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  await Task.findByIdAndRemove(req.params.id);
+  res.status(204).send();
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
